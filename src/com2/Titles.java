2211a1adb15bd00d176e3d0017b174f92e6f41a6
Leonardo Narciso <LNarcisoOV@gmail.com>
2024-04-16 19:17:37 -0300

    Add capitain example.

---
+++ src/com2/Titles.java
@@ -0,0 +1,13 @@
+package com2;
+
+public class Titles {
+
+    public static void main(String[] args) {
+
+        
+        System.out.println("Capitain name: " + args[0]);
+        System.out.println("Soldier name: " + args[1]);
+
+    }
+
+}
